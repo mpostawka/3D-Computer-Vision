932625a1f8ed5fc7f2e00563d470237f41927a01
mpostawka <mpostawka@gmail.com>
2019-11-28 09:54:09 +0100

    better build file

--- RobustCircleFitting/build.sh
+++ RobustCircleFitting/build.sh
@@ -1,5 +1,5 @@
 g++ -c -g -I./include src/MatrixReaderWriter.cpp
-g++ -c -g -Wall -I./include src/Model.cpp
-g++ -c -g -Wall -I./include src/RANSAC.cpp
-g++ -c -g -Wall -I./include main.cpp
-g++ MatrixReaderWriter.o Model.o RANSAC.o main.o -g -Wall $(pkg-config --cflags --libs opencv) -o main
+g++ -c -g -Wall -I./include $(pkg-config --cflags opencv4) src/Model.cpp
+g++ -c -g -Wall -I./include $(pkg-config --cflags opencv4) src/RANSAC.cpp
+g++ -c -g -Wall -I./include $(pkg-config --cflags opencv4) main.cpp
+g++ MatrixReaderWriter.o Model.o RANSAC.o main.o -g -Wall $(pkg-config --cflags --libs opencv4) -o main
